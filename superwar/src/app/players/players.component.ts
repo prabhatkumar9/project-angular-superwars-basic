35d28da71c16579258fea10c16b2a68aab126e04
prabhatkumar9 <prabhatvisvakarma9@gmail.com>
2020-04-23 05:53:30 +0530

    done

--- superwar/src/app/players/players.component.ts
+++ superwar/src/app/players/players.component.ts
@@ -71,10 +71,17 @@ export class PlayersComponent implements OnInit {
       .filter(player => player.type == type)
       .map(
         player =>
-          `<div class="player">
-      <img src="${player.image}" alt=" ">
-      <div class="name">${player.name}</div>
-      <div class="strength">${player.strength}</div>
+          `<div style="border: 2px solid gray;
+          border-radius: 5%;
+          box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.2);
+          margin: 10px;
+          padding: 2rem;
+          text-align: center;
+          width: 80px;">
+      <img style="width: 100%;
+      height: 50%;" src="${player.image}" alt=" ">
+      <div style="text-align: center" class="name">${player.name}</div>
+      <div style="font-size: 3em" class="strength">${player.strength}</div>
   </div>`
       )
       .join('');
